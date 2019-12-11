b88d1c35b3f225210ed15a0efd49ced18690a2b7
TatsukiFukushima <tatsuki2222net@gmail.com>
2019-12-11 15:25:53 +0900

    素因数分解できた

--- numbers.go
+++ numbers.go
@@ -6,14 +6,16 @@ import (
 	"time"
 )
 
-func main() {
+// どこまで乱数が進んだかを記憶
+var z1Last = big.NewInt(2)
+var z2Last = big.NewInt(2)
 
+func main() {
 	var nS string
 	fmt.Printf("n = ")
 	fmt.Scan(&nS)
 	n, _ := new(big.Int).SetString(nS, 10)
 
-	var results []*big.Int
 	if n.ProbablyPrime(20) {
 		fmt.Println("素数です")
 	} else {
@@ -21,66 +23,43 @@ func main() {
 		start := time.Now()
 		for {
 			calcFactor := calcFactor(n)
-			results = append(results, calcFactor)
+			fmt.Printf(calcFactor.String() + ", ")
 			n.Div(n, calcFactor)
 			if n.ProbablyPrime(20) {
-				results = append(results, n)
+				fmt.Println(n)
 				break
 			}
 		}
 
-		fmt.Println(results)
+		// fmt.Println(results)
 		end := time.Now()
 		fmt.Printf("%fsec\n", (end.Sub(start)).Seconds())
 	}
 }
 
 func calcFactor(n *big.Int) *big.Int {
-	z1 := big.NewInt(2)
-	z2 := big.NewInt(2)
+	z1 := z1Last
+	z2 := z2Last
 	z2_z1 := big.NewInt(0)
 	a := big.NewInt(1)
 	b := big.NewInt(1)
 	result := big.NewInt(1)
-	zero := big.NewInt(0)
 	one := big.NewInt(1)
-	twe := big.NewInt(2)
-	three := big.NewInt(3)
-	five := big.NewInt(5)
-	thrteen := big.NewInt(13)
-	module := big.NewInt(0)
+	numbers := []*big.Int{big.NewInt(2), big.NewInt(3), big.NewInt(5), big.NewInt(7), big.NewInt(11), big.NewInt(13)}
 
-	// 2対策
-	n.DivMod(n, twe, module)
-	if module.Cmp(zero) == 0 {
-		n.Mul(n, twe)
-		return twe
+	// 小さい値が素因数だとたまにエラーが起こるので対策
+	for _, number := range numbers {
+		if isModZero(n, number) {
+			return number
+		}
 	}
-	n.Mul(n, twe).Add(n, module)
 
-	// 3対策
-	n.DivMod(n, three, module)
-	if module.Cmp(zero) == 0 {
-		n.Mul(n, three)
-		return three
+	if z1.Cmp(z2) != 0 {
+		result.GCD(a, b, z2_z1.Sub(z2, z1).Abs(z2_z1), n)
+		if result.Cmp(one) > 0 {
+			return result
+		}
 	}
-	n.Mul(n, three).Add(n, module)
-
-	// 5対策
-	n.DivMod(n, five, module)
-	if module.Cmp(zero) == 0 {
-		n.Mul(n, five)
-		return five
-	}
-	n.Mul(n, five).Add(n, module)
-
-	// 13対策
-	n.DivMod(n, thrteen, module)
-	if module.Cmp(zero) == 0 {
-		n.Mul(n, thrteen)
-		return thrteen
-	}
-	n.Mul(n, thrteen).Add(n, module)
 
 	for {
 		z1.Mul(z1, z1)
@@ -99,9 +78,23 @@ func calcFactor(n *big.Int) *big.Int {
 		}
 	}
 	if result.ProbablyPrime(20) {
+		z1Last.Set(z1)
+		z2Last.Set(z2)
 		return result
 	} else {
-		fmt.Println(result)
 		return calcFactor(result)
 	}
+}
+
+// isModZero 余りがゼロかどうかを判定
+func isModZero(n, m *big.Int) bool {
+	zero := big.NewInt(0)
+	module := big.NewInt(0)
+	n.DivMod(n, m, module)
+	if module.Cmp(zero) == 0 {
+		n.Mul(n, m)
+		return true
+	}
+	n.Mul(n, m).Add(n, module)
+	return false
 }
